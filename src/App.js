880d739fe6b846c78cab9aba1aef9d3c12bbfaaa
Eva GC <evarteca@gmail.com>
2022-05-26 19:53:18 +0200

    routes, pages & components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+//!IMPORTS:
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import Home from "./pages/Home";
+import ProductList from './pages/ProductList';
+import Error from './pages/Error';
+import NotFound from './pages/NotFound';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
+//!MAIN FUNCTION:
 function App() {
+
+  //RENDER VIEW:
   return (
     <div className="App">
-      <h1>Mercadona</h1>
+
+      <Navbar />
+
+      <Routes >  
+
+       <Route path="/" element={ <Home /> } />
+       <Route path="/product-list" element={ <ProductList /> } />
+        
+
+       <Route path="/error" element={ <Error/> } />
+       <Route path="*" element={ <NotFound /> } />  
+
+      </Routes>
+
+      <Footer />
+
     </div>
   );
 }
