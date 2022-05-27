24d44e1dcd04ff598be97bce85909f9bc893ddaf
Eva GC <evarteca@gmail.com>
2022-05-27 22:29:48 +0200

    cards for each product in each category

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,8 +1,18 @@
 //!IMPORTS:
-import { Paper, Container } from "@mui/material";
+import {
+  Paper,
+  Container,
+  Typography,
+  Box,
+  Grid,
+  Collapse,
+  Card,
+  CardContent,
+  CardActions,
+  Divider,
+} from "@mui/material";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Box, Grid, Collapse } from "@mui/material/";
 import SyncLoader from "react-spinners/SyncLoader";
 import AddForm from "../components/AddForm";
 import axios from "axios";
@@ -10,15 +20,15 @@ import axios from "axios";
 //!MAIN FUNCTION:
 
 function ProductList() {
-    //CONSTANTS & HOOKS:
-  const [allProducts, setAllProducts] = useState(null);  
+  //CONSTANTS & HOOKS:
+  const [allProducts, setAllProducts] = useState(null);
   const [showForm, setShowForm] = useState(false);
-  const {id} = useParams()
+  const { id } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
     getAllProducts();
-    console.log("A ver:", allProducts, id)
+    console.log("A ver:", allProducts, id);
   }, []);
 
   //!INTERNAL FUNCTIONS:
@@ -31,17 +41,17 @@ function ProductList() {
       );
       console.log(response.data.categories);
       const data = response.data.categories;
-      data.forEach((eachProduct)=>{
-          //console.log(eachProduct, eachProduct.id, id)
-          eachProduct.id == id && setAllProducts(eachProduct.products);
-      })
+      data.forEach((eachProduct) => {
+        //console.log(eachProduct, eachProduct.id, id)
+        eachProduct.id == id && setAllProducts(eachProduct.products);
+      });
     } catch (err) {
       navigate("/error");
     }
   };
 
-   //LOADING SYSTEM:
-   if (!allProducts) {
+  //LOADING SYSTEM:
+  if (!allProducts) {
     return (
       <div className="loadingSpinner">
         <h2>...Loading...</h2>
@@ -53,41 +63,131 @@ function ProductList() {
 
   //RENDER VIEW:
   return (
-    <Container component={"main"} maxWidth={"sm"} sx={{ my: 5 }}>
-        <div>
-          <button onClick={() => setShowForm(!showForm)}>           
-            {showForm ? <b>HIDE FORM</b>  : <b>ADD PRODUCT</b> }
-          </button>
-          <Collapse in={showForm}>            
-            <AddForm />
-          </Collapse>
-        </div>
-
-        <Paper
-          variant={"outlined"}
-          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+    <Container component={"main"} maxWidth={"sm"} sx={{ my: 2 }}>
+      <Box
+        sx={{
+          flexWrap: "wrap-reverse",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          pt: 10,
+        }}
+      >
+        <Typography
+          sx={{ fontSize: 30, fontWeight: "bold" }}
+          color="text.primary"
+          gutterBottom
+          align={"center"}
         >
-          {allProducts.map((eachProduct, index) => {
-            return (
-              <Grid item xs={12} sm={6} key={eachProduct.id}>
-                <Box
-                  sx={{
-                    p: 2,
-                    my: 2,
-                    border: "2px solid",
-                    borderColor: "#26b879",
-                    borderRadius: "5px",
-                    boxShadow: "#26b879",
-                  }}
-                >
-                  {eachProduct.display_name}
-                </Box>
-              </Grid>
-            );
-          })}
-        </Paper>
-      </Container>
-  )
+          Chocolates tipoo...
+        </Typography>
+      </Box>
+      <div>
+        <button onClick={() => setShowForm(!showForm)}>
+          {showForm ? <b>HIDE FORM</b> : <b>ADD PRODUCT</b>}
+        </button>
+        <Collapse in={showForm}>
+          <AddForm />
+        </Collapse>
+      </div>
+
+      <Box
+        sx={{
+          flexWrap: "wrap-reverse",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          pt: 10,
+        }}
+      >
+        {allProducts.map((eachProduct, index) => {
+          return (
+            <Card
+              variant={"outlined"}
+              sx={{ p: 3, m: 3, borderColor: "#26b879" }}
+              key={eachProduct.id}
+            >
+              <CardContent sx={{ p: 1 }}>
+                <div>
+                  <div>
+                    <img
+                      src={eachProduct.thumbnail}
+                      alt={eachProduct.display_name}
+                      width="50%"
+                    />
+                    <CardActions
+                      sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-around",
+                      }}
+                    >
+                      <button>Lupita</button>
+                    </CardActions>
+                  </div>
+                  <div>
+                    <Typography
+                      sx={{ fontSize: 16, fontWeight: "bold" }}
+                      color="black"
+                      gutterBottom
+                      align={"center"}
+                    >
+                      {eachProduct.display_name}
+                    </Typography>
+                    <Divider />
+                    <Typography variant="body2" sx={{ my: 1 }}>
+                      Precio: {eachProduct.price_instructions.unit_price} €
+                    </Typography>
+                    <Divider />
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </Box>
+
+      <Paper
+        variant={"outlined"}
+        sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+      >
+        {allProducts.map((eachProduct, index) => {
+          return (
+            <Grid item xs={12} sm={6} key={eachProduct.id}>
+              <Box
+                sx={{
+                  p: 2,
+                  my: 2,
+                  border: "2px solid",
+                  borderColor: "#26b879",
+                  borderRadius: "5px",
+                  boxShadow: "#26b879",
+                }}
+              >
+                <CardContent sx={{ p: 1 }}>
+                  <Typography
+                    sx={{ fontSize: 16, fontWeight: "bold" }}
+                    gutterBottom
+                    align={"center"}
+                  >
+                    {eachProduct.display_name}
+                  </Typography>
+                  <Divider />
+                  <Typography
+                    sx={{ fontSize: 16, fontWeight: "bold" }}
+                    gutterBottom
+                    align={"center"}
+                  >
+                    Precio: {eachProduct.price_instructions.unit_price} €
+                  </Typography>
+                </CardContent>
+              </Box>
+            </Grid>
+          );
+        })}
+      </Paper>
+    </Container>
+  );
 }
 
-export default ProductList
+export default ProductList;
