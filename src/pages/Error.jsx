c84f316ac13bb982af76947d782900eb90517ad0
Eva GC <evarteca@gmail.com>
2022-05-29 11:15:40 +0200

    Error 500 page & Not Found page customized

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,34 @@
+//!IMPORTS:
+import { Box, Typography } from "@mui/material";
+import GppBadIcon from "@mui/icons-material/GppBad";
 
-
+//!MAIN FUNCTION:
 function Error() {
+  //RENDER VIEW:
   return (
-    <div>Error</div>
-  )
+    <div className="App">
+      <Box
+        sx={{
+          flexWrap: "wrap-reverse",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          pt: 10,
+        }}
+      >
+        <Typography
+          sx={{ fontSize: 16, fontWeight: "bold", pb: 30 }}
+          color="text.primary"
+          gutterBottom
+          align={"center"}
+        >
+          <GppBadIcon /> <br /> <br />
+          ¡Vaya! parece que estamos teniendo algún problema interno... <br />
+          ¡Vuelve más tarde!
+        </Typography>
+      </Box>
+    </div>
+  );
 }
 
-export default Error
+export default Error;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,38 @@
+//!IMPORTS:
+import { Box, Typography } from "@mui/material";
+import GppBadIcon from "@mui/icons-material/GppBad";
 
+//!MAIN FUNCTION:
 
 function NotFound() {
+  //RENDER VIEW:
   return (
-    <div>NotFound</div>
-  )
+    <div className="App">
+      <Box
+        sx={{
+          flexWrap: "wrap-reverse",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          pt: 10,
+        }}
+      >
+        <Typography
+          sx={{ fontSize: 16, fontWeight: "bold", pb: 30 }}
+          color="text.primary"
+          gutterBottom
+          align={"center"}
+        >
+          <GppBadIcon /> <br /> <br />
+          ¡Vaya!
+          <br />
+          Parece que la dirección a la que tratas de acceder no existe...
+          <br />
+          ¡Prueba con otra!
+        </Typography>
+      </Box>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
